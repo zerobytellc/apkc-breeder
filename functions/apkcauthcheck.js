93fc0d0009f91a52c60d860a74746ae25d3a1dba
Timothy C. Sweeney-Fanelli <tim@zerobytellc.com>
2021-08-04 15:54:57 -0400

    Auth check in serverless function

--- functions/apkcauthcheck.js
+++ functions/apkcauthcheck.js
@@ -1,11 +1,19 @@
-exports.handler = async () => {
+exports.handler = async (event, context) => {
     console.log('Function ran...');
 
     const data = { name: 'mario', age: 35, job: 'plumber' };
 
-    // return response to browser:
-    return {
-        status: 200,
-        body: JSON.stringify(data),
+    if (context.clientContext.user) {
+        // return response to browser:
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data),
+        }
+    } else {
+        return {
+            statusCode: 401,
+            body: JSON.stringify({msg: 'You must be logged in to see this.' } ),
+        }
     }
+
 }
--- pages/guides.js
+++ pages/guides.js
@@ -1,9 +1,31 @@
 import styles from '../styles/Guides.module.css'
+import {useContext, useEffect} from "react";
+import AuthContext from "../stores/authContext";
+
+//https://www.youtube.com/watch?v=wizwky_4YTs
 
 export default function Guides() {
+    const { user, authReady } = useContext(AuthContext)
+
+    useEffect(() => {
+        if ( authReady ) {
+            fetch('/.netlify/functions/apkcauthcheck', user && {
+                headers: {
+                    Authorization: 'Bearer ' + user.token.access_token,
+                }
+            })
+                .then(res => res.json())
+                .then(data => console.log(data));
+
+            return () => {
+            };
+        }
+    }, [user, authReady]);
+
+
   return (
     <div className={styles.guides}>
       <h2>All Guides</h2>
-    </div> 
+    </div>
   )
 }
