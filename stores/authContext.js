deb13c711fe700bbe35d583460644fddf4608893
Timothy C. Sweeney-Fanelli <tim@zerobytellc.com>
2021-08-04 14:55:37 -0400

    AuthReady in navbar

--- stores/authContext.js
+++ stores/authContext.js
@@ -10,6 +10,7 @@ const AuthContext = createContext({
 
 export const AuthContextProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const [authReady, setAuthReady] = useState(false);
 
     useEffect(() => {
         netlifyIdentity.on('login', (user) => {
@@ -21,6 +22,12 @@ export const AuthContextProvider = ({ children }) => {
         netlifyIdentity.on('logout', () => {
             setUser(null);
             console.log('Logout event');
+        })
+
+        netlifyIdentity.on('init', (user) => {
+            setUser(user);
+            setAuthReady(true);
+            console.log( 'Init event' );
         })
 
         // init netlify identity connection
@@ -40,7 +47,7 @@ export const AuthContextProvider = ({ children }) => {
         netlifyIdentity.logout();
     }
 
-    const context = { user, login, logout };
+    const context = { user, login, logout, authReady };
 
     return (
         <AuthContext.Provider value={context}>
