a541bf7ac3cf9111d175800404d8e299e270c6a8
Timothy C. Sweeney-Fanelli <tim@zerobytellc.com>
2021-08-04 14:01:07 -0400

    Initial commit.

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,14 +6,14 @@ export default function Navbar() {
     <div className="container">
       <nav>
         <Image src="/rupee.png" width={50} height={48} />
-        <h1>Gaming Vibes</h1>
+        <h1>APKC Breeder</h1>
         <ul>
           <li><Link href="/"><a>Home</a></Link></li>
           <li><Link href="/guides"><a>Guides</a></Link></li>
         </ul>
       </nav>
       <div className="banner">
-        <Image src="/banner.png" width={966} height={276} />
+        <Image src="/puppy-banner.jpg" width={966} height={505} />
       </div>
     </div>
   )
