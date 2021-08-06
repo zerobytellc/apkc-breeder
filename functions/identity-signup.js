af9cc3c45cc44de10350efb05be676ba7d515e66
Timothy C. Sweeney-Fanelli <tim@zerobytellc.com>
2021-08-06 09:07:13 -0400

    Adding identity-* function templates for initial testing

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -1,7 +1,25 @@
 exports.handler = async (event, context) => {
-    console.log('New user signup... ');
+    const body = JSON.parse(req.body)
 
-    return {
-        statusCode: 200,
+    const eventType = body.event
+    const user = body.user
+    const email = user.email
+
+    let roles = []
+
+    if (eventType === 'signup') {
+        roles.push('breeder');
+
+        console.log(`User: ${user.id} signed-up and given roles: ${roles}`)
+        console.log(user, `User: ${user.id} details`)
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ app_metadata: { roles: roles } }),
+        }
+    } else {
+        return {
+            statusCode: 200,
+        }
     }
 }
