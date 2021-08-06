f108c84578ad0d8c535cfa83f2f050bfbf951fdc
Timothy C. Sweeney-Fanelli <tim@zerobytellc.com>
2021-08-06 09:36:56 -0400

    Adding identity-* function templates for initial testing

--- functions/identity-login.js
+++ functions/identity-login.js
@@ -1,7 +1,31 @@
 exports.handler = async (event, context) => {
-    console.log('New user login... ' );
+    const body = JSON.parse(req.body)
 
-    return {
-        statusCode: 200,
+    const eventType = body.event
+    const user = body.user
+    const email = user.email
+
+
+    let roles = [];
+    if ( user.app_metadata.roles ) {
+        roles.push(user.app_metadata.roles);
+    }
+
+    if (eventType === 'login') {
+        if ( roles.includes("breeder") ) {
+            console.log( "User: ${user.id} logged in and already has role 'breeder'" );
+        } else {
+            roles.push('breeder');
+            console.log( "User: ${user.id} logged in and granted role 'breeder'" );
+        }
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ app_metadata: { roles: roles } }),
+        }
+    } else {
+        return {
+            statusCode: 200,
+        }
     }
 }
