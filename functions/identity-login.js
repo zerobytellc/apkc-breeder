0c5c5a1a61b8124a2aa49cad961a28b64d1cea0c
Timothy C. Sweeney-Fanelli <tim@zerobytellc.com>
2021-08-23 11:35:17 -0400

    Creating user if doesn't exist

--- functions/identity-login.js
+++ functions/identity-login.js
@@ -19,7 +19,7 @@ exports.handler = async (req, context) => {
                 console.log( "I found a user!" );
             } else {
                 console.log( "This must be a new user ... let's create the entry" );
-                client.query(q.Create(q.Ref("users"), user))
+                await client.query(q.Create(q.Ref("users"), user))
                     .then((response) => {
                         console.log( "Successfully created the new user... " + JSON.stringify(response.data));
                     })
