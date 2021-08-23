d0dbbd38d18b1007380e93ebe93a747f5c7b8cba
Timothy C. Sweeney-Fanelli <tim@zerobytellc.com>
2021-08-23 13:31:00 -0400

    Creating user if doesn't exist

--- functions/identity-login.js
+++ functions/identity-login.js
@@ -16,7 +16,7 @@ exports.handler = async (req, context) => {
         .then(async (response) => {
             const results = response.data;
             if ( results.length > 0 ) {
-                await client.query( q.Get(results) ).then( (response) => {
+                await client.query( q.Get( JSON.parse(results)) ).then( (response) => {
                     console.log( "User: " + response );
                 }, (error) => {
                     console.log( "Outchie." + error);
