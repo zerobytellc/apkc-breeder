b619e9c4598ac30358558590a1b3734b85fca64e
Timothy C. Sweeney-Fanelli <tim@zerobytellc.com>
2021-08-23 09:59:47 -0400

    First successful faunadb query -- testing server side

--- functions/apkcauthcheck.js
+++ functions/apkcauthcheck.js
@@ -7,10 +7,11 @@ const client = new faunadb.Client({
 
 exports.handler = async (event, context) => {
     console.log('Function !! ran...');
-    client.query(q.Paginate(q.Match(q.Ref("indexes/all_users"))))
+    await client.query(q.Paginate(q.Match(q.Ref("indexes/all_users"))))
         .then((response) => {
             console.log( "All Users: " + response.data );
-        }, (error) => {console.log( "uh oh ... " + error );})
+        }, (error) => {console.log( "uh oh ... " + error );}
+
 
     const data = { name: 'mario', age: 35, job: 'plumber' };
 
