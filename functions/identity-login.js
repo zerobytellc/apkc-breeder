2b57055b67d2f8fe3e377da82b20649d38adcc99
Timothy C. Sweeney-Fanelli <tim@zerobytellc.com>
2021-08-23 13:15:28 -0400

    Creating user if doesn't exist

--- functions/identity-login.js
+++ functions/identity-login.js
@@ -16,8 +16,7 @@ exports.handler = async (req, context) => {
         .then(async (response) => {
             const results = response.data;
             if ( results.length > 0 ) {
-                const dbUser = JSON.parse(results);
-                console.log( "I found a user! ID: " + dbUser.id );
+                console.log( "I found a user!" + results );
             } else {
                 console.log( "This must be a new user ... let's create the entry" );
 
