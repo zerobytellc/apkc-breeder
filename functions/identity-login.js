53c4535a67c6642c9397122ae6a35ec5ddafaa7f
Timothy C. Sweeney-Fanelli <tim@zerobytellc.com>
2021-08-24 09:49:45 -0400

    Reverting test changes to identity-login

--- functions/identity-login.js
+++ functions/identity-login.js
@@ -12,49 +12,49 @@ exports.handler = async (req, context) => {
     const user = body.user
     const email = user.email
 
-    await client.query(q.Match(q.Ref("indexes/users_by_email"), email))
-           .then((result) => {
-              console.log( q.Get(result) );
-           })
+    await client.query(q.Paginate(q.Match(q.Ref("indexes/users_by_email"), email)))
+        .then(async (response) => {
+            const results = response.data;
+            if ( results.length > 0 ) {
+                console.log( "Existing user" );
 
-    let users = [{email: 'he'}];
-    if (users.length > 0) {
-        console.log("I found a user! " + users);
-    } else {
-        console.log("This must be a new user ... let's create the entry");
+            } else {
+                console.log( "This must be a new user ... let's create the entry" );
 
-        const userItem = {
-            data: user
-        }
-        console.log("Creating userItem: " + userItem);
-        await client.query(q.Create(q.Ref("classes/users"), userItem))
-            .then((response) => {
-                console.log("Successfully created the new user... ");
-            })
-    }
-
-    console.log("Database User Entry: " + JSON.stringify(results));
+                const userItem = {
+                    data: user
+                }
+                console.log( "Creating userItem: " + userItem );
+                await client.query(q.Create(q.Ref("classes/users"), userItem))
+                    .then((response) => {
+                        console.log( "Successfully created the new user... " );
+                    })
+            }
+            console.log("Database User Entry: " + JSON.stringify(results));
+        }, (error) => {
+            console.log("uh oh ... " + error);
+        })
 
     console.log(user, `${user.id} details`);
-    if (context.ClientContext && context.ClientContext.user)
+    if ( context.ClientContext && context.ClientContext.user )
         console.log('UserContext: ' + context.ClientContext.user)
 
     let roles = [];
-    if (user.app_metadata.roles) {
+    if ( user.app_metadata.roles ) {
         roles.push(user.app_metadata.roles);
     }
 
     if (eventType === 'login') {
-        if (user.app_metadata.roles.includes("breeder")) {
-            console.log(`User ${user.id} logged in and already had role 'breeder'`);
+        if ( user.app_metadata.roles.includes("breeder") ) {
+            console.log( `User ${user.id} logged in and already had role 'breeder'` );
         } else {
             user.app_metadata.roles.push('breeder');
-            console.log(`User: ${user.id} logged in and granted role 'breeder'`);
+            console.log( `User: ${user.id} logged in and granted role 'breeder'` );
         }
 
         return {
             statusCode: 200,
-            body: JSON.stringify({app_metadata: {roles: user.app_metadata.roles}}),
+            body: JSON.stringify({ app_metadata: { roles: user.app_metadata.roles } }),
         }
     } else {
         return {
